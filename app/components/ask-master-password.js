eeef6b00cd1bdad36e71c225aeaf98412f162f24
Alejandro Blanco <alejandro.b.e@gmail.com>
2015-08-15 19:56:04 +0200

    Bugfixes in ask master pwd modal

--- app/components/ask-master-password.js
+++ app/components/ask-master-password.js
@@ -5,11 +5,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     showModal: Ember.observer('flag', function () {
-        var $modal;
+        var that = this,
+            $modal;
 
         if (this.get('flag')) {
             this.set('masterPassword', '');
-            $modal = Ember.$('#master-password-modal').modal({ show: false });
+            $modal = Ember.$('#master-password-modal').modal({
+                show: false
+            });
+            $modal.off('hidden.bs.modal').on('hidden.bs.modal', function () {
+                that.set('flag', false);
+            });
             $modal.modal('show');
             $modal.find('input').focus();
         }
