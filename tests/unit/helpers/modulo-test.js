f89e56bd76dd7484a614706916c57798573282c3
Alejandro Blanco <alejandro.b.e@gmail.com>
2016-02-13 20:44:52 +0100

    Fix responsive columns in password list

---
+++ tests/unit/helpers/modulo-test.js
@@ -0,0 +1,10 @@
+import { modulo } from '../../../helpers/modulo';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | modulo');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = modulo(42);
+  assert.ok(result);
+});
