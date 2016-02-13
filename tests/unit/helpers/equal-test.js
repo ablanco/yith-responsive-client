d868eaeaded806da820aa769dd1ca1503d3bb722
Alejandro Blanco <alejandro.b.e@gmail.com>
2016-02-13 20:06:41 +0100

    Add proper resets to responsive columns

---
+++ tests/unit/helpers/equal-test.js
@@ -0,0 +1,10 @@
+import { equal } from '../../../helpers/equal';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | equal');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = equal(42);
+  assert.ok(result);
+});
